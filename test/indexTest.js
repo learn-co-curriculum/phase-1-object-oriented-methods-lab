3eee94e95c98f0f572c6df3e18e712e2cfb5ae49
drakeltheryuujin <kellye.greene@flatironschool.com>
2018-08-31 18:10:37 -0400

    Fixes out of date version and specs

--- test/indexTest.js
+++ test/indexTest.js
@@ -17,30 +17,50 @@ describe('boardMembers', function() {
   })
 
   describe('veto()', function() {
+    let polishedBoardMember;
+    before(() => {
+      polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+    })
     it('returns "No, I must disagree"', function() {
       expect(polishedBoardMember.veto()).to.equal("No, I must disagree")
     })
   })
 
   describe('approve()', function() {
+    let polishedBoardMember;
+    before(() => {
+      polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+    })
     it('returns "You can do that!"', function() {
       expect(polishedBoardMember.approve()).to.equal("You can do that!")
     })
   })
 
   describe('doCharity()', function() {
+    let polishedBoardMember;
+    before(() => {
+      polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+    })
     it('returns "I like to help people."', function() {
       expect(polishedBoardMember.doCharity()).to.equal("I like to help people.")
     })
   })
 
   describe('releasePressStatement()', function() {
+    let polishedBoardMember;
+    before(() => {
+      polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+    })
     it('returns "You will see great things from Scuber."', function() {
       expect(polishedBoardMember.releasePressStatement()).to.equal("You will see great things from Scuber.")
     })
   })
 
   describe('sayHi()', function() {
+    let polishedBoardMember;
+    before(() => {
+      polishedBoardMember = new BoardMember("Mr. Polished", "New York", "law")
+    })
     it('returns "Hi, my name is <name>. I am from <homestate>, and I was trained in <training>."', function() {
       expect(polishedBoardMember.sayHi()).to.equal("Hi, my name is Mr. Polished. I am from New York, and I was trained in law.")
     })
