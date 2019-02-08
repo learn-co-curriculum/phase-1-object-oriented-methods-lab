4239c231bdcd861e6092beea32090c3959868fd6
cjbrock <corinna@flatironschool.com>
2019-02-08 14:44:22 -0500

    add solution

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  veto(){
+  this.veto = function(){
     return 'No, I must disagree'
   }
-  approve(){
+  this.approve = function(){
     return 'You can do that!'
   }
 
-  doCharity(){
+  this.doCharity = function(){
     return "I like to help people."
   }
 
-  releasePressStatement(){
+  this.releasePressStatement = function(){
     return "You will see great things from Scuber."
   }
 
-  sayHi(){
+  this.sayHi = function(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
