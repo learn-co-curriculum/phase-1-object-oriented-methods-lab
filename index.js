277306c5762b96cd0f5c41b68fc4862c6d85f6ae
Ann Duong <39737217+thuyanduong-flatiron@users.noreply.github.com>
2019-05-21 15:54:07 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,22 +3,22 @@ function BoardMember(name, homeState, training){
   this.homeState = homeState
   this.training = training
 
-  this.veto = function(){
+  veto(){
     return 'No, I must disagree'
   }
-  this.approve = function(){
+  approve(){
     return 'You can do that!'
   }
 
-  this.doCharity = function(){
+  doCharity(){
     return "I like to help people."
   }
 
-  this.releasePressStatement = function(){
+  releasePressStatement(){
     return "You will see great things from Scuber."
   }
 
-  this.sayHi = function(){
+  sayHi(){
     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
   }
 }
